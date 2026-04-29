75a4e7334a9daa4e50074c4a3e1a130e9512eae5
jayvohn-santoso <jayvohn.santoso@gmail.com>
2026-04-29 10:53:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,43 +7,33 @@ import pandas
 import random
 
 # -------------- COLLECTING DATA -------------- #
-letter_templates = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+letter_templates = [
+    "letter_templates/letter_1.txt", 
+    "letter_templates/letter_2.txt",
+    "letter_templates/letter_3.txt"]
+
 today = dt.datetime.now()
+
 df = pandas.read_csv("birthdays.csv")
 
 # -------------- CHECKING BIRTHDAYS -------------- #
 for i in range(len(df)):
-    if df.month[i] == today.month and df.day[i] == today.day:
+    if int(df.month[i]) == today.month and int(df.day[i]) == today.day:
         random_letter = random.choice(letter_templates)
 
         # -------------- REPLACING NAME -------------- #
         with open(random_letter, "r") as file:
             content = file.read()
 
-        new_content = content.replace("[NAME]", df.name[i])
-
-        with open(random_letter, "w") as file:
-            file.write(new_content)
-
-        with open(random_letter, "r") as file:
-            final_letter = file.read()
-
+        letter_content = content.replace("[NAME]", df.name[i])
+        
         # -------------- SENDING EMAILS -------------- #
         MY_EMAIL = os.environ.get("MY_EMAIL")
         MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-        with smtplib.SMTP("smtp.gmail.com") as connection:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=MY_PASSWORD)
             connection.sendmail(from_addr=MY_EMAIL,
-                                to_addrs=dt.email[i],
-                                msg=f"Subject: Birthday Wisher\n\n{final_letter}")
-
-        # -------------- RETURNING ORIGINAL TEXT -------------- #
-        with open(random_letter, "r") as file:
-            edited_content = file.read()
-
-        original_content = edited_content.replace(df.name[i], "[NAME]")
-
-        with open(random_letter, "w") as file:
-            file.write(original_content)
+                                to_addrs=df.email[i],
+                                msg=f"Subject: Birthday Wisher\n\n{letter_content}")
